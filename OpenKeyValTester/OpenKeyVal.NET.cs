ddb0fe4cf08573094ee756ba1013277b4973913d
Jeremy <jeremy@weebu.com>
2013-08-10 15:26:05 -0700

    Initial

---
+++ OpenKeyValTester/OpenKeyVal.NET.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OpenKeyValTester
+{
+    public class OpenKeyValDotNET
+    {
+        private string _address = "http://api.openkeyval.org/";        
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public async Task<string> SaveAsync<T>(string key, T value)
+        {
+            var valueAsJson = JsonConvert.SerializeObject(value);
+            using (var client = new WebClient())
+            {
+                var result = await client.UploadValuesTaskAsync(Address, "POST", new NameValueCollection { { key, valueAsJson } });
+                return Encoding.ASCII.GetString(result);
+            }
+        }
+
+        public string Save<T>(string key, T value)
+        {
+            var valueAsJson = JsonConvert.SerializeObject(value);
+
+            using (var client = new WebClient())
+            {
+                var result = client.UploadValues(Address, "POST", new NameValueCollection { { key, valueAsJson } });
+                return Encoding.ASCII.GetString(result);
+            }
+        }
+
+        public async Task<string> SaveStringAsync(string key, string value)
+        {
+            using (var client = new WebClient())
+            {
+                var result = await client.UploadValuesTaskAsync(Address, "POST", new NameValueCollection { { key, value } });
+                return Encoding.ASCII.GetString(result);
+            }
+        }
+
+        public string SaveString<T>(string key, T value)
+        {
+            var valueAsJson = JsonConvert.SerializeObject(value);
+            using (var client = new WebClient())
+            {
+                var result = client.UploadValues(Address, "POST", new NameValueCollection { { key, valueAsJson } });
+                return Encoding.ASCII.GetString(result);
+            }
+        }    
+
+        /// <summary>
+        /// Async method that returns the value for a key that has been deserialized into type T
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public async Task<T> GetAsync<T>(string key)
+        {
+            using (var client = new WebClient())
+            {
+                var response = await client.DownloadStringTaskAsync(Address + key);
+                return JsonConvert.DeserializeObject<T>(response);
+            }       
+        }
+
+        /// <summary>
+        /// Synchronous method that returns the value for a key that has been deserialized into type T
+        /// </summary>
+        /// <typeparam name="T">The type to deserialize into.</typeparam>
+        /// <param name="key">The key to retrieve</param>
+        /// <returns></returns>
+        public T Get<T>(string key)
+        {
+            using (var client = new WebClient())
+            {
+                var response = client.DownloadString(Address + key);
+                return JsonConvert.DeserializeObject<T>(response);
+            }           
+        }
+
+        /// <summary>
+        /// Synchronous method that returns the value for a key that has been deserialized into type T
+        /// </summary>
+        /// <param name="key">The key to retrieve</param>
+        /// <returns></returns>
+        public string GetString(string key)
+        {
+            using (var client = new WebClient())
+            {
+                var response = client.DownloadString(Address + key);
+                return response;
+            }
+        }
+
+        /// <summary>
+        /// Synchronous method that returns the value for a key that has been deserialized into type T
+        /// </summary>
+        /// <param name="key">The key to retrieve</param>
+        /// <returns></returns>
+        public string Get(string key)
+        {
+            using (var client = new WebClient())
+            {
+                return client.DownloadString(Address + key);
+            }
+        }
+
+        /// <summary>
+        /// Async method that returns the value for a key as a JSON string.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public async Task<string> GetStringAsync(string key)
+        {
+            using (var client = new WebClient())
+            {
+                var response = await client.DownloadStringTaskAsync(Address + key);
+                return response;
+            }    
+        }
+    }
+}
---
+++ OpenKeyValTester/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace OpenKeyValTester
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                var tester = new OpenKeyValTester();
+                var t = new Task(async () =>
+                    {
+                        await tester.RunTests();
+                    });
+                t.Start();
+                t.Wait();
+                
+                Console.WriteLine("Done.");
+                Console.ReadLine();
+            }
+            catch (WebException we)
+            {
+                Console.WriteLine("WebException: Message");
+                Console.WriteLine(we.Message);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception:");
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class OpenKeyValTester
+    {
+        private readonly List<string> _keys = new List<string>();
+        private readonly OpenKeyValDotNET _okv = new OpenKeyValDotNET();
+
+        public async Task RunTests()
+        {
+            await DoPostAsync();
+
+
+            DoGetString();
+            DoGetWithType();
+        }
+
+        public void DoPost()
+        {
+            var guid = ConfigurationManager.AppSettings.Get("Guid");
+            if (String.IsNullOrEmpty(guid))
+            {
+                guid = Guid.NewGuid().ToString();
+                ConfigurationManager.AppSettings.Set("Guid", guid);
+            }
+
+            var keyPrefix = String.Format("weebu-{0}", guid);
+
+            var key1 = String.Format("{0}-{1}", keyPrefix, "1");
+            var value1 = new PostInfo
+            {
+                PostID = 3,
+                DateTime = DateTime.Now,
+                Data = "API is coming along!!"
+            };
+
+            _keys.Add(key1);
+
+            _okv.Save(key1, value1);
+        }
+
+        public async Task DoPostAsync()
+        {
+            var guid = ConfigurationManager.AppSettings.Get("Guid");
+            if (String.IsNullOrEmpty(guid))
+            {
+                guid = Guid.NewGuid().ToString();
+                ConfigurationManager.AppSettings.Set("Guid", guid);
+            }
+
+            var keyPrefix = String.Format("weebu-{0}", guid);
+
+            var key1 = String.Format("{0}-{1}", keyPrefix, "1");
+            var value1 = new PostInfo
+            {
+                PostID = 3,
+                DateTime = DateTime.Now,
+                Data = "API is coming along!!"
+            };
+
+            _keys.Add(key1);
+
+            await _okv.SaveAsync(key1, value1);
+        }
+
+        public async Task DoGetStringAsync()
+        {
+            foreach (var key in _keys)
+            {
+                var r = await _okv.GetAsync<PostInfo>(key);
+                var output = String.Format("PostID: '{0}', DateTime: '{1}', Data: '{2}'", r.PostID, r.DateTime, r.Data);
+                Console.WriteLine(output);
+            }
+        }
+
+        public void DoGetString()
+        {
+            foreach (var key in _keys)
+            {
+                var r = _okv.Get<PostInfo>(key);
+                var output = String.Format("PostID: '{0}', DateTime: '{1}', Data: '{2}'", r.PostID, r.DateTime, r.Data);
+                Console.WriteLine(output);
+            }
+        }
+
+        public async Task DoGetWithTypeAsync()
+        {
+            foreach (var key in _keys)
+            {
+                var r = await _okv.GetAsync<PostInfo>(key);
+                var output = String.Format("PostID: '{0}', DateTime: '{1}', Data: '{2}'", r.PostID, r.DateTime, r.Data);
+                Console.WriteLine(output);
+            }
+        }
+
+        public void DoGetWithType()
+        {
+            foreach (var key in _keys)
+            {
+                var r = _okv.Get<PostInfo>(key);
+                var output = String.Format("PostID: '{0}', DateTime: '{1}', Data: '{2}'", r.PostID, r.DateTime, r.Data);
+                Console.WriteLine(output);
+            }
+        }
+    }
+
+    public class PostInfo
+    {
+        public int PostID { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Data { get; set; }
+    }
+
+}
